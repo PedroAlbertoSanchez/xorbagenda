f9c16af4b2bbaf7581cd6af3619ee716366e4057
admin <admin@pwc1>
2018-02-15 17:12:39 +0100

    aaa

---
+++ Spring11-MVCHibernate/src/main/java/com/ejemplos/spring/config/ApplicationContextConfig.java
@@ -0,0 +1,27 @@
+package com.ejemplos.spring.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.ejemplos.spring.model.User;
+
+
+
+@Configuration
+@ComponentScan("")
+@EnableTransactionManagement
+public class ApplicationContextConfig {
+
